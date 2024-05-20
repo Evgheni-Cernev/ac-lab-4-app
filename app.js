5627cd289474d38481dd546aa5f6508cdcfc7ffe
evgheni.cernev <evgheni.cernev@klarna.com>
2024-05-20 07:33:58 +0300

    added cloud bould config files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is my containerized Express app.');
+  res.send('Hello, World! Stage');
 });
 
 app.listen(port, () => {
